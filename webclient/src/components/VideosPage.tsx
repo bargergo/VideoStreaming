04e0e6c00d01f0f032f8ab1ffece572c36dd7c71
bargergo <bargergo@gmail.com>
2021-05-13 10:32:23 +0200

    Can list videos on client

--- webclient/src/components/VideosPage.tsx
+++ webclient/src/components/VideosPage.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+
+interface VideInfo {
+  id: string;
+  title: string;
+}
+
+async function getVideoInfos(): Promise<VideInfo[]> {
+  const response = await fetch("/api/files")
+    .then(r => r.json());
+  return response;
+}
 
 const VideosPage = () => {
+
+  const [videos, setVideos] = useState<VideInfo[]>([]);
+
+  useEffect(() => {
+    getVideoInfos()
+      .then(results => {
+        setVideos(results);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    return;
+  }, [])
+
   return (
     <div>
       This is Videos Page!
+      <ul>
+        {videos.map(video => <li key={video.id}><Link to={`video/${video.id}`}>{video.title}</Link></li>)}
+      </ul>
     </div>
   )
 }
