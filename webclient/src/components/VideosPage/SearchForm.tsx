1d5427bd57653f6dba1e85a8e95a582136e7dd87
bargergo <bargergo@gmail.com>
2021-11-13 14:25:47 +0100

    Search for video design

--- webclient/src/components/VideosPage/SearchForm.tsx
+++ webclient/src/components/VideosPage/SearchForm.tsx
@@ -15,16 +15,14 @@ const SearchForm = ({onSearch, onShowAll}) => {
   }
 
   return (
-  <Form className="mb-3" onSubmit={submit}>
-    <Form.Group as={Row} className="mb-3" controlId="exampleForm.ControlInput1">
-      <Form.Label column sm="2">Search for video</Form.Label>
-      <Col>
+    <Form className="mb-3" onSubmit={submit}>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label sm="2">Search for video</Form.Label>
         <Form.Control type="text" placeholder="Title" onChange={(event) => setSearchText(event.target.value)} value={searchText}/>
-      </Col>
-    </Form.Group>
-    <Button type="submit" variant="primary">Search</Button>{' '}
-    <Button type="button" variant="outline-primary" onClick={showAll}>Show all videos</Button>
-  </Form>
+      </Form.Group>
+      <Button type="submit" variant="primary">Search</Button>{' '}
+      <Button type="button" variant="outline-primary" onClick={showAll}>Show all videos</Button>
+    </Form>
   );
 };
 
