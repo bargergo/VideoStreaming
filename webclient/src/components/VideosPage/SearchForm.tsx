7b3f49462dbefb443e4a9c5d17f5703d03927b7f
bargergo <bargergo@gmail.com>
2021-11-13 14:26:32 +0100

    Remove unused imports

--- webclient/src/components/VideosPage/SearchForm.tsx
+++ webclient/src/components/VideosPage/SearchForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Col, Form, Row } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 
 const SearchForm = ({onSearch, onShowAll}) => {
 
