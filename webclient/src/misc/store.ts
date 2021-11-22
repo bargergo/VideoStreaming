10742443965b367b1591a1cac027190c5536049e
bargergo <bargergo@gmail.com>
2021-11-22 21:58:52 +0100

    Store JWT token, username in session storage

--- webclient/src/misc/store.ts
+++ webclient/src/misc/store.ts
@@ -1,10 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
 
+const loadState = () => {
+  try {
+    const serializedState = sessionStorage.getItem('state');
+
+    if (serializedState === null) {
+      return undefined;
+    }
+
+    return JSON.parse(serializedState);
+  } catch (error) {
+    return undefined;
+  }
+};
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    sessionStorage.setItem('state', serializedState);
+  } catch (error) {
+    // Ignore write errors.
+  }
+};
+
 const store = configureStore({
   reducer: {
     user: userReducer
   },
+  preloadedState: loadState()
+});
+
+store.subscribe(() => {
+  saveState(store.getState());
 });
 
 export default store;
