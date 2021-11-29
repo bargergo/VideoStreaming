bdfe95ba8c67ee523155d09e43e953c316317747
bargergo <bargergo@gmail.com>
2021-11-29 18:02:54 +0100

    Add admin user

--- user_service/src/main/kotlin/hu/bme/aut/user_service/SetupDataLoader.kt
+++ user_service/src/main/kotlin/hu/bme/aut/user_service/SetupDataLoader.kt
@@ -1,16 +1,21 @@
 package hu.bme.aut.user_service
 
 import hu.bme.aut.user_service.dao.RoleRepository
+import hu.bme.aut.user_service.dao.UserRepository
 import hu.bme.aut.user_service.model.Role
+import hu.bme.aut.user_service.model.User
 import org.springframework.context.ApplicationListener
 import org.springframework.context.event.ContextRefreshedEvent
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 import javax.transaction.Transactional
 
 
 @Component
 class SetupDataLoader(
-    private val roleRepository: RoleRepository
+    private val roleRepository: RoleRepository,
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
 ) : ApplicationListener<ContextRefreshedEvent> {
 
     var alreadySetup = false
@@ -20,8 +25,9 @@ class SetupDataLoader(
         if (alreadySetup)
             return
 
-        createRoleIfNotFound("ROLE_ADMIN")
-        createRoleIfNotFound("ROLE_USER")
+        val adminRole = createRoleIfNotFound("ROLE_ADMIN")
+        val userRole = createRoleIfNotFound("ROLE_USER")
+        createAdminUserIfNotFound(adminRole, userRole)
 
         alreadySetup = true
     }
@@ -34,5 +40,17 @@ class SetupDataLoader(
             roleRepository.save(role)
         }
         return role
+    }
+
+    @Transactional
+    fun createAdminUserIfNotFound(vararg roles: Role): User {
+        var user: User? = userRepository.findByUsername("admin")
+        if (user == null) {
+            user = User(null, "admin", passwordEncoder.encode("admin"), mutableListOf(
+                *roles
+            ))
+            userRepository.save(user)
+        }
+        return user
     }
 }
